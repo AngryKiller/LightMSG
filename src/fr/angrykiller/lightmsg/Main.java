da196e28caf143f53a7fd02d74616f9003ee9a0a
AngryKiller <stevelabus59@gmail.com>
2016-10-25 23:57:23 +0200

    Overhaul of the code
    Overhaul of the message sending system.

--- src/fr/angrykiller/lightmsg/Main.java
+++ src/fr/angrykiller/lightmsg/Main.java
@@ -7,61 +7,73 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+
 import net.md_5.bungee.api.ChatColor;
-import fr.angrykiller.lightmsg.ActionBar;
 public class Main extends JavaPlugin implements Listener{
 	boolean actionbarConfig = false;
-	FileConfiguration config = this.getConfig();
-	
-	public void onEnable(){
-		saveDefaultConfig();
-		Bukkit.getPluginManager().registerEvents(this, this);
-		
+	protected static FileConfiguration config;
+	public void loadConfig() {
+	      config = this.getConfig();
+		  config.options().copyDefaults(true);
+	      this.saveDefaultConfig();
 	}
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[]args)
-	{
+	public void onEnable(){
+		loadConfig();
+		Bukkit.getPluginManager().registerEvents(this, this);
+		getLogger().info("Plugin started!");
+		getLogger().info("ActionBar is: "+config.getBoolean("actionbar")+"!");
+		getLogger().info("Language is:"+config.getString("language")+" !");
+	}
+
+	
+
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
 		Player p = (Player) sender;
-		
 		if(label.equalsIgnoreCase("msg"))
 		{
-			String joueur = args[0];
-			String pvmessagebrut = "";
-			
-			for(int i = 1; i < args.length; i++)
-			{
-				pvmessagebrut = pvmessagebrut + " " + args[i];
-			}
-			if(pvmessagebrut.isEmpty()){
-				p.sendMessage(ChatColor.RED+"Veuillez envoyer un message valide.");
-			}
-			else{
-			try{
-				String pvmessagecolored = ChatColor.translateAlternateColorCodes('&', pvmessagebrut);
-				Player receiver = Bukkit.getServer().getPlayer(joueur);
-				receiver.sendMessage(ChatColor.RED+"["+p.getName()+"] --> [Vous] :"+ChatColor.WHITE+pvmessagecolored);
-				p.sendMessage(ChatColor.RED+"[Vous] --> ["+joueur+"] :" +ChatColor.WHITE+pvmessagecolored);
-				if(config.getBoolean("actionbar")){
-			    ActionBar actionBar = new ActionBar("§7"+p.getName()+" vous a envoyé un message:"+ChatColor.WHITE+pvmessagecolored);
-			    actionBar.sendToPlayer(receiver);
+			String targetName =  args[0];
+			String message = "";
+			if(args.length == 0){
+				p.sendMessage("Utilisation: /msg <joueur> <message>");
+				return false;
+			}else{
+			if(args.length > 1){
+			if(Bukkit.getServer().getPlayerExact(targetName) == null){
+				p.sendMessage(ChatColor.RED+"Le joueur "+targetName+" n'est pas connecté !");
+				return false;
+			}else{
+				for(int i = 1; i != args.length; i++){
+					message += args[i]+" ";
 				}
-				else{}
-			
-			}catch(NullPointerException npe)
-			{
-				p.sendMessage(ChatColor.RED+"Le joueur "+joueur+" n'est pas connecté");
+				Player target = (Player) Bukkit.getServer().getPlayer(targetName);
+				String messagecolored = ChatColor.translateAlternateColorCodes('&', message);
+				String targetPrefix = ChatColor.GRAY+"["+ChatColor.RED+p.getName()+ChatColor.GRAY+"] "+ChatColor.GOLD+"--> "+ChatColor.GRAY+"["+ChatColor.RED+"Moi"+ChatColor.GRAY+"]"+ChatColor.WHITE+": ";
+				String senderPrefix = ChatColor.GRAY+"["+ChatColor.RED+"Moi"+ChatColor.GRAY+"] "+ChatColor.GOLD+"--> "+ChatColor.GRAY+"["+ChatColor.RED+p.getName()+ChatColor.GRAY+"]"+ChatColor.WHITE+": ";
+				String targetActionBarMessage = ChatColor.RED+p.getName()+ChatColor.GOLD+" vous a envoyé un message: "+ChatColor.WHITE+messagecolored;
+				target.sendMessage(targetPrefix+messagecolored);
+				p.sendMessage(senderPrefix+messagecolored);
+				if(config.getBoolean("actionbar")){
+				ActionBar targetActionBar = new ActionBar(targetActionBarMessage);	
+				targetActionBar.sendToPlayer(target);
+				}
 			}
+			}else{
+				p.sendMessage(ChatColor.RED+"Veuillez spécifier un message !");
+				return false;
 			}
-			
+
+			}
 		}
-		
 		return true;
-	}
+		}
+	
 	
 	public void onDisable(){
 		getLogger().info("Plugin stopped successfully !");
 	}
 	public void onServerReload(){
-		getLogger().info("Plugin reloadé!");
-		this.reloadConfig();
+		getLogger().info("Plugin reloaded!");
+		loadConfig();
 	}
 }
